a4c0bd1e0710263e46253b4cfb098ef37a613039
Sky <skydebreuil@gmail.com>
2016-05-28 20:15:05 +0900

    lattice

--- Assets/Scripts/SCR_LatticeJoint.cs
+++ Assets/Scripts/SCR_LatticeJoint.cs
@@ -6,8 +6,45 @@ public class SCR_LatticeJoint : MonoBehaviour
 	public SCR_LatticePoint A;
 	public SCR_LatticePoint B;
 
-	public void Update () 
+	public float Distance;
+	public float Stiffness = 1;
+
+	public bool Active = true;
+
+	public void Start()
 	{
-	
+		if(A == null || B == null)
+		{
+			return;
+		}
+
+		Distance = (A.transform.position - B.transform.position).magnitude;
+	}
+
+	public void LateUpdate () 
+	{
+		if(A == null || B == null)
+		{
+			return;
+		}
+
+		var diff = A.transform.position - B.transform.position;
+		var length = diff.magnitude;
+
+		var norm = length == 0 ? Vector3.up : diff / length;
+
+		if(Active)
+		{
+			var push = length - Distance;
+
+			var impulse = (norm * push) * Stiffness * Time.deltaTime;
+
+			A.Velocity -= impulse;
+			B.Velocity += impulse;
+		}
+
+		transform.position = (A.transform.position + B.transform.position) * 0.5F;
+		transform.rotation = Quaternion.LookRotation(norm);
+		transform.localScale = new Vector3(1, 1, length * 0.5F);
 	}
 }
